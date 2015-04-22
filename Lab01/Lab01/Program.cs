0b4cf9f2050d7606ca65527f57880290c5748079
Mauro Gimenez <mauro88rc@gmail.com>
2015-04-21 22:45:26 -0300

    1er commit: Unidad1_Labs

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // el modificador estatico hace que el metodo sea estatico o de clase
+            // el modificador void indica que el metodo no retorno ningun tipo o valor
+            // el parametro args es un arreglo del tipo string
+            // este metodo sera el punto de entrada de la aplicacion, que sera lo primero que se ejecutara
+            // todas las aplicaciones tendran un Main, cuya ubicacion y estructura dependera del tipo de proyecto
+            System.Console.Write("Hola Mundo\n");
+        }
+    }
+}
